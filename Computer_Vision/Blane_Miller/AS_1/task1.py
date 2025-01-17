9bce465b1a61ef4e54d24464a01d9e3f9d9e45cb
blanemiller1 <130030338+blanemiller1@users.noreply.github.com>
2025-01-16 19:58:07 -0700

    Blane added comments to task2 code

--- Computer_Vision/Blane_Miller/AS_1/task1.py
+++ Computer_Vision/Blane_Miller/AS_1/task1.py
@@ -26,22 +26,22 @@ with open('datafile.txt', 'r') as f:
     #Using eval to convert items from string to list
     b = eval(f.read()) 
 
-#Task 1: Finding the maximum
+#Exercise 1: Finding the maximum
 #Using preset max finder function
 maximum = max(b)
 print(f"The maximum of this data set is {maximum}")
 
-#Task 2: Finding the minimum
+#Exercise 2: Finding the minimum
 #Using preset min finder function
 minimum = min(b)
 print(f"The minimum of this data set is {minimum}")
 
-#Task 3: Finding the index where the number 38 is located
+#Exercise 3: Finding the index where the number 38 is located
 #Using preset index finder function
 idx = b.index(38)
 print(f"The index of the number 38 in this data set is {idx}")
 
-#Task 4: Finding the number or numbers that are repeated most, and how many times they are repeated
+#Exercise 4: Finding the number or numbers that are repeated most, and how many times they are repeated
 #Importing a counter so I can count repetitions of a word
 from collections import Counter
 #Counting the number of instances of each number in the list
@@ -52,7 +52,7 @@ max_counts = max(ind_counts.values())
 most_repeated_num = [num for num, count in ind_counts.items() if count == max_counts]
 print(f"The most repeated number(s) in this data set is/are {most_repeated_num}, which repeats {max_counts} times")
 
-#Task 5: Sorting the list
+#Exercise 5: Sorting the list
 #Importing np stuff 
 import numpy as np
 #Converting list to numpy array
@@ -61,7 +61,7 @@ arr = np.array(b)
 sorted_arr = np.sort(arr)
 print(f"The sorted list is as shown: {sorted_arr}")
 
-#Task 6: List of the even numbers, sorted
+#Exercise 6: List of the even numbers, sorted
 #Using list comprehension to extract the even numbers 
 even_nums = [num for num in b if num % 2 == 0]
 #Sorting list
--- Computer_Vision/Blane_Miller/AS_1/task2.py
+++ Computer_Vision/Blane_Miller/AS_1/task2.py
@@ -14,19 +14,27 @@ Hardware Setup: Power on the Pi -> either connect peripherials or connect via Pi
 Example Excecution: -> open terminal -> navigate to the directory where the task1.py file is located using cd command ->
 run the python file using 'python task1.py' command 
 ''' 
+
+# Function to gather user input
 def beginning():
     user_str = input("Your string is:")
+    # Adds a space at the end of the string so that the entire string, 
+    # including the final element, will be parsed through
     return user_str + ' '
 
+# Function call outside the loop so that the string is only gathered once
 the_str = beginning()
 
-
+# Initial state searching for an 'a'
 def state0(new_char):
     if new_char == 'a':
         return state1 
     else:
         return state0
     
+# Second state searching for a 'b', 
+# or maintaining on an 'a', 
+# or going back to initial state otherwise
 def state1(new_char):
     if new_char == 'a':
         return state1
@@ -35,6 +43,10 @@ def state1(new_char):
     else:
         return state0
 
+
+# Third state searching for a 'c', 
+# or pushing back to the second state if an 'a', 
+# or pushing back to initial state otherwise
 def state2(new_char):
     if new_char == 'a':
         return state1
@@ -43,6 +55,9 @@ def state2(new_char):
     else: 
         return state0
 
+# Fourth state searching for a 'd', 
+# or pushing back to the second state if an 'a', 
+# or pushing back to initial state otherwise
 def state3(new_char):
     if new_char == 'a':
         return state1
@@ -50,18 +65,26 @@ def state3(new_char):
         return state4 
     else: 
         return state0
-    
+
+# Final state printing that abcd is contained in the string,
+# and then going back to the initial state to check for another instance of it
 def state4(new_char):
     print("abcd is contained in the string")
-    return None
+    return state0
 
-
+# Setting state to be initial state
 state = state0
+# Setting check to be false in case abcd is not contained in the string
 check = False
+# Looping through every element of the string
 for i in the_str: 
+    # Putting the current state into a variable, 
+    # and then setting that as the current state in the machine
     new_state = state(i)
     state = new_state
+    # Setting check to True so that the not contained text doesn't output
     if state == state4:
         check = True
+# Using the check variable in case abcd isn't contained within the string
 if check == False:
     print("abcd is not contained in the string")
