7f6eb29e3760a6d07238cf25a8b686b721e58751
kobeprior99 <129470871+kobeprior99@users.noreply.github.com>
2025-01-26 12:11:30 -0700

    aruco for pi

--- Computer_Vision/Kobe_Prior/AS2/examples/aruco.py
+++ Computer_Vision/Kobe_Prior/AS2/examples/aruco.py
@@ -5,7 +5,13 @@ from time import sleep
 
 myDict = aruco.getPredefinedDictionary(aruco.DICT_6X6_50)
 myArucoImg = aruco.generateImageMarker(myDict, 0, 400)
-cv.imshow("Aruco", myArucoImg)
-cv.imwrite("aruco.png", myArucoImg)
-cv.waitKey(0)
-cv.destroyAllWindows()
+# cv.imshow("Aruco", myArucoImg)
+# cv.imwrite("aruco.png", myArucoImg)
+# cv.waitKey(0)
+# cv.destroyAllWindows()
+camera = cv.VideoCapture(0)
+sleep(.5)
+ret, frame = camera.read()
+grey = cv.cvtColor(frame, cv.COLOR_BGR2GRAY) #make the image greyscale for aruco detection
+cv.imshow("overlay", grey)
+camera.release()
