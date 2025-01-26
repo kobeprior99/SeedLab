73fd2098b99563e3fa5214abd163446895abd5b4
kobeprior99 <129470871+kobeprior99@users.noreply.github.com>
2025-01-26 12:39:41 -0700

    push to test if it works on pi

--- Computer_Vision/Kobe_Prior/AS2/examples/aruco.py
+++ Computer_Vision/Kobe_Prior/AS2/examples/aruco.py
@@ -1,29 +1,29 @@
-import cv2 as cv
+import cv2
 from cv2 import aruco
 import numpy as np
 from time import sleep
 
 myDict = aruco.getPredefinedDictionary(aruco.DICT_6X6_50)
 myArucoImg = aruco.generateImageMarker(myDict, 0, 400)
-# cv.imshow("Aruco", myArucoImg)
-# cv.imwrite("aruco.png", myArucoImg)
-# cv.waitKey(0)
-# cv.destroyAllWindows()
-camera = cv.VideoCapture(0)
+# cv2.imshow("Aruco", myArucoImg)
+# cv2.imwrite("aruco.png", myArucoImg)
+# cv2.waitKey(0)
+# cv2.destroyAllWindows()
+camera = cv2.VideoCapture(0)
 sleep(.5)
 ret, frame = camera.read()
-grey = cv.cvtColor(frame, cv.COLOR_BGR2GRAY) #make the image greyscale for aruco detection
-# cv.imshow("overlay", grey)
-corners, ids, rejected =aruco.detectMarkers(grey, myDict)
+grey = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY) #make the image greyscale for aruco detection
+# cv2.imshow("overlay", grey)
+corners, ids, rejected = aruco.detectMarkers(grey, myDict)
 
-overlay = cv.cvtColor(grey, cv.COLOR_GRAY2RGB)
+overlay = cv2.cvtColor(grey, cv2.COLOR_GRAY2RGB)
 overlay = aruco.drawDetectedMarkers(overlay, corners, borderColor = 4)
 if not ids is None:
     ids = ids.flatten()
     for (outline, id) in zip(corners, ids):
         markerCorners = outline.reshape((4,2))
-        overlay = cv.putText(overlay, str(id),(int(markerCorners[0,0]), int(markerCorners[0,1]) - 15),cv.FONT_HERSHEY_SIMPLEX,0.5, (255,0,0), 2)
-cv.imshow("overlay",overlay)
-cv.waitKey(0)
-cv.destroyAllWindows()
+        overlay = cv2.putText(overlay, str(id),(int(markerCorners[0,0]), int(markerCorners[0,1]) - 15),cv2.FONT_HERSHEY_SIMPLEX,0.5, (255,0,0), 2)
+cv2.imshow("overlay",overlay)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
 camera.release()
