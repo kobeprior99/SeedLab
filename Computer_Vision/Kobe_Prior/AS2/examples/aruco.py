3efab75b7790bfcc9c4f64b11fea058c074af95d
kobeprior99 <129470871+kobeprior99@users.noreply.github.com>
2025-01-26 12:02:42 -0700

    aruco added

--- Computer_Vision/Kobe_Prior/AS2/examples/aruco.py
+++ Computer_Vision/Kobe_Prior/AS2/examples/aruco.py
@@ -3,3 +3,9 @@ from cv2 import aruco
 import numpy as np
 from time import sleep
 
+myDict = aruco.getPredefinedDictionary(aruco.DICT_6X6_50)
+myArucoImg = aruco.generateImageMarker(myDict, 0, 400)
+cv.imshow("Aruco", myArucoImg)
+cv.imwrite("aruco.png", myArucoImg)
+cv.waitKey(0)
+cv.destroyAllWindows()
