6fd54ac35a6be7d293a22684e29db91631a19b17
blanemiller1 <130030338+blanemiller1@users.noreply.github.com>
2025-01-27 14:53:34 -0700

    Blane: conitnuation of previous

--- Computer_Vision/Blane_Miller/AS_2/task_2a.py
+++ Computer_Vision/Blane_Miller/AS_2/task_2a.py
@@ -33,11 +33,11 @@ while(True):
             overlay = cv2.putText(overlay, str(id),(int(markerCorners[0,0]), int(markerCorners[0,1]) - 15),cv2.FONT_HERSHEY_SIMPLEX,0.5, (255,0,0), 2) 
         lcd.clear()
         sleep(0.1)
-        lcd.message = f"The id is {id}"
+        lcd.print(f"The id is {id}")
     else:
         lcd.clear()
         sleep(0.1)
-        lcd.message = "No markers found"
+        lcd.print("No markers found")
     cv2.imshow("overlay",overlay)
     k = cv2.waitKey(1) & 0xFF
     if k == ord('q'):
