7786849eddc1f6ba05d80c922eb6609ad008476f
blanemiller1 <130030338+blanemiller1@users.noreply.github.com>
2025-01-26 18:10:31 -0700

    Blane: tried to use two separate i2cs

--- Computer_Vision/Blane_Miller/AS_2/task_1b.py
+++ Computer_Vision/Blane_Miller/AS_2/task_1b.py
@@ -6,14 +6,15 @@ import adafruit_character_lcd.character_lcd_rgb_i2c as character_lcd
 
 # I2C Setup
 ARD_ADDR = 8
-i2c = SMBus(1)
+i2c1 = SMBus(1)
 
 # LCD Setup (adjust pins based on your wiring)
 lcd_columns = 16  # Number of columns in your LCD
 lcd_rows = 2      # Number of rows in your LCD
 
 
-lcd = character_lcd.Character_LCD_RGB_I2C(i2c, lcd_columns, lcd_rows)
+i2c2 = board.I2C()
+lcd = character_lcd.Character_LCD_RGB_I2C(i2c2, lcd_columns, lcd_rows)
 
 while True:
     # Input an integer to send
@@ -23,13 +24,13 @@ while True:
             break
 
         # Send the integer to Arduino
-        i2c.write_byte_data(ARD_ADDR,0,integer_value)
+        i2c1.write_byte_data(ARD_ADDR,0,integer_value)
 
         # Wait for the Arduino to process the integer
         sleep(0.1)
 
         # Read the modified integer from the Arduino
-        modified_int = i2c.read_byte(ARD_ADDR)
+        modified_int = i2c1.read_byte(ARD_ADDR)
 
         print(f"Received from Arduino: {modified_int}")
 
