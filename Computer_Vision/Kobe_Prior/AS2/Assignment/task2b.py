c0ab5679b9d65e51ad92bfac4e3d9cf93ea7905f
kobeprior99 <129470871+kobeprior99@users.noreply.github.com>
2025-01-27 23:20:01 -0700

    test green param

--- Computer_Vision/Kobe_Prior/AS2/Assignment/task2b.py
+++ Computer_Vision/Kobe_Prior/AS2/Assignment/task2b.py
@@ -23,11 +23,11 @@ from time import sleep
 # mask_green function that takes in an image and returns a mask for the green color
 def mask_green(img):
     # [64, 140, 73] value using colorThresholdFinder.py from tutorial
-    # green hue between 110 and 170 degrees
+    # green hue between 100 and 140 degrees
     # saturation    0-255
     # value         25-255
-    lowergreen = np.array([55, 25, 25])
-    uppergreen = np.array([85, 255, 255])
+    lowergreen = np.array([50, 25, 25])
+    uppergreen = np.array([70, 255, 255])
     # convert the image to hsv
     img_hsv = cv.cvtColor(img, cv.COLOR_BGR2HSV)
     #create a binary mask for the green color
