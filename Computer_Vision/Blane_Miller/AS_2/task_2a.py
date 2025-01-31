1ec47198b45ecc32c0660bdd2bd9717d5b1743de
blanemiller1 <130030338+blanemiller1@users.noreply.github.com>
2025-01-31 14:54:26 -0700

    Blane: commented 2a code

--- Computer_Vision/Blane_Miller/AS_2/task_2a.py
+++ Computer_Vision/Blane_Miller/AS_2/task_2a.py
@@ -15,6 +15,7 @@ Example Excecution: -> open terminal -> navigate to the directory where the task
 run the python file using 'python task1.py' command and point the camera towards the something that may contain green.
 '''
 
+#import libraries
 import cv2
 from cv2 import aruco
 import numpy as np
@@ -23,6 +24,7 @@ import board
 import time
 import adafruit_character_lcd.character_lcd_rgb_i2c as character_lcd
 
+#lcd setup
 lcd_columns = 16
 lcd_rows = 2 
 
@@ -30,22 +32,27 @@ i2c = board.I2C()
 
 lcd = character_lcd.Character_LCD_RGB_I2C(i2c, lcd_columns, lcd_rows) 
 
+#camera setup
 camera = cv2.VideoCapture(0)
 sleep(.5)
 
+#setup variables to let lcd only update upon seeing something new
 last_marker_id = None
 message_displayed = None
 
+#inf loop for picture taking until waitkey press
 while(True):
-    ret, frame = camera.read()
+    ret, frame = camera.read() 
 
-    myDict = aruco.getPredefinedDictionary(aruco.DICT_6X6_50)
+    myDict = aruco.getPredefinedDictionary(aruco.DICT_6X6_50) # setup aruco dict
     grey = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)  # Convert to grayscale for ArUco detection
 
+    #setup overlay display params
     corners, ids, rejected = aruco.detectMarkers(grey, myDict)
     overlay = cv2.cvtColor(grey, cv2.COLOR_GRAY2RGB)
     overlay = aruco.drawDetectedMarkers(overlay, corners, borderColor=4)
 
+    #Look for arucos
     if ids is not None: 
         ids = ids.flatten()
         
@@ -71,6 +78,8 @@ while(True):
             overlay = cv2.putText(overlay, str(id), 
                                   (int(markerCorners[0,0]), int(markerCorners[0,1]) - 15),
                                   cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255,0,0), 2) 
+            
+    # Make sure that the message starts as No markers found if no aruco is immediately detected
     else:
         if message_displayed != "No markers found":
             lcd.clear()
@@ -78,6 +87,7 @@ while(True):
             message_displayed = "No markers found"
             last_marker_id = None
 
+    #show overlay from earlier and get waitkey for inf loop break setup
     cv2.imshow("overlay", overlay)
     k = cv2.waitKey(1) & 0xFF
     if k == ord('q'):
