5e9b2b8eb4e232a57278ec7da200a0973a3f629e
kobeprior99 <129470871+kobeprior99@users.noreply.github.com>
2025-01-31 14:37:27 -0700

    added id2.png to examples

--- Computer_Vision/Kobe_Prior/AS2/examples/aruco.py
+++ Computer_Vision/Kobe_Prior/AS2/examples/aruco.py
@@ -4,29 +4,29 @@ import numpy as np
 from time import sleep
 
 myDict = aruco.getPredefinedDictionary(aruco.DICT_6X6_50)
-myArucoImg = aruco.generateImageMarker(myDict, 1, 400)
+myArucoImg = aruco.generateImageMarker(myDict, 2, 400)
 # cv2.imshow("Aruco", myArucoImg)
-cv2.imwrite("aruco_id1.png", myArucoImg)
+cv2.imwrite("id2.png", myArucoImg)
 # cv2.waitKey(0)
 # cv2.destroyAllWindows()
-camera = cv2.VideoCapture(0)
-sleep(.5)
-ret, frame = camera.read()
-grey = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY) #make the image greyscale for aruco detection
-# cv2.imshow("overlay", grey)
-corners, ids, rejected = aruco.detectMarkers(grey, myDict)
+# camera = cv2.VideoCapture(0)
+# sleep(.5)
+# ret, frame = camera.read()
+# grey = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY) #make the image greyscale for aruco detection
+# # cv2.imshow("overlay", grey)
+# corners, ids, rejected = aruco.detectMarkers(grey, myDict)
 
-overlay = cv2.cvtColor(grey, cv2.COLOR_GRAY2RGB)
-overlay = aruco.drawDetectedMarkers(overlay, corners, borderColor = 4)
-if not ids is None:
-    ids = ids.flatten()
-    for (outline, id) in zip(corners, ids):
-        markerCorners = outline.reshape((4,2))
-        overlay = cv2.putText(overlay, str(id),(int(markerCorners[0,0]), int(markerCorners[0,1]) - 15),cv2.FONT_HERSHEY_SIMPLEX,0.5, (255,0,0), 2)
-cv2.imshow("overlay",overlay)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
-camera.release()
+# overlay = cv2.cvtColor(grey, cv2.COLOR_GRAY2RGB)
+# overlay = aruco.drawDetectedMarkers(overlay, corners, borderColor = 4)
+# if not ids is None:
+#     ids = ids.flatten()
+#     for (outline, id) in zip(corners, ids):
+#         markerCorners = outline.reshape((4,2))
+#         overlay = cv2.putText(overlay, str(id),(int(markerCorners[0,0]), int(markerCorners[0,1]) - 15),cv2.FONT_HERSHEY_SIMPLEX,0.5, (255,0,0), 2)
+# cv2.imshow("overlay",overlay)
+# cv2.waitKey(0)
+# cv2.destroyAllWindows()
+# camera.release()
 # print(corners[0][0])
 # print(corners[0][0][0])
 # print(corners[0][0][0][0])
