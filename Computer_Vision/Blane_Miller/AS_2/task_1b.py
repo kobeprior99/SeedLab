81feb80a094c8cdc2fd6c08ddc7aac8280db98c7
blanemiller1 <130030338+blanemiller1@users.noreply.github.com>
2025-02-05 19:38:11 -0700

    Blane: whatever the hell I changed I don't remember

--- Computer_Vision/Blane_Miller/AS_2/task_1b.py
+++ Computer_Vision/Blane_Miller/AS_2/task_1b.py
@@ -1,3 +1,20 @@
+'''
+*******************************************************************
+* File Name         : task_1b.py
+* Description       : have the pi send an integer to the arduino, 
+send back that plus 100 to the pi, 
+have the lcd screen display that modded integer
+*                    
+* Revision History  :
+* Date		Author 			Comments
+* ------------------------------------------------------------------
+* 01/25/2025	Blane Miller	Created File
+*
+******************************************************************
+Hardware Setup: Power on the Pi -> either connect peripherials or connect via PiConnect or other VNC service --> connect web cam
+Example Excecution: -> open terminal -> navigate to the directory where the task_1b.py file is located using cd command ->
+run the python file using 'python task1.py' command and point the camera towards the something that may contain green.
+'''
 from smbus2 import SMBus
 from time import sleep
 import board
@@ -8,11 +25,9 @@ import adafruit_character_lcd.character_lcd_rgb_i2c as character_lcd
 ARD_ADDR = 8
 i2c1 = SMBus(1)
 
-# LCD Setup (adjust pins based on your wiring)
-lcd_columns = 16  # Number of columns in your LCD
-lcd_rows = 2      # Number of rows in your LCD
-
-
+# LCD Setup 
+lcd_columns = 16  
+lcd_rows = 2      
 i2c2 = board.I2C()
 lcd = character_lcd.Character_LCD_RGB_I2C(i2c2, lcd_columns, lcd_rows)
 
@@ -35,7 +50,7 @@ while True:
         print(f"Received from Arduino: {modified_int}")
 
         # Display the modified integer on the LCD
-        lcd.clear()  # Clear any previous text
+        lcd.clear()  
         lcd.message = f"Modified Int:\n{modified_int}"
 
     except IOError:
