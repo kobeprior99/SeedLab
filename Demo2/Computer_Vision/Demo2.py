665c831d7a4ef4ea9b3eee14ca6f18df130e2964
kobeprior99 <129470871+kobeprior99@users.noreply.github.com>
2025-03-05 14:27:33 -0700

    Demo2: Test1

--- Demo2/Computer_Vision/Demo2.py
+++ Demo2/Computer_Vision/Demo2.py
@@ -66,6 +66,7 @@ UPPER_GREEN = np.array([85, 255, 255])
 FX = cameraMatrix[0,0] # focal length in pixels
 CX = cameraMatrix[0,2] # camera center in pixels
 
+MARKER_WIDTH_IRL = 2 #inches
 #aruco dictrionary
 MY_DICT = aruco.getPredefinedDictionary(aruco.DICT_6X6_50) # setup aruco dict
 
@@ -75,7 +76,6 @@ ARD_ADDR = 8 #set arduino address
 i2c_arduino = SMBus(1)#initialize i2c bus to bus 1
 
 # constant width of aruco marker in inches
-X = 2.0 #inches
 
 # LCD Dimensions and I2C
 lcd_columns = 16
@@ -87,8 +87,119 @@ LCDqueue = queue.Queue()
 endQueue = False #flag to end inf loop in LCD display
 
 
+def find_mask(frame):
+    #convert to frame to hsv
+    hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+    green_mask = cv2.inRange(hsv, LOWER_GREEN, UPPER_GREEN)
+    #GaussianBlur takes less resources than two morphological operations
+    green_mask = cv2.GaussianBlur(green_mask, (5,5), 0)
+    red1_mask = cv2.inRange(hsv, LOWER_RED1, UPPER_RED1)
+    red2_mask = cv2.inRange(hsv, LOWER_RED2, UPPER_RED2)
+    #add the upper and lower masks
+    red_mask = cv2.bitwise_or(red1_mask, red2_mask)
+    red_mask = cv2.GaussianBlur(red_mask, (5,5), 0)
+    #debug to refine masks
+    #cv2.imshow('red', red_mask)
+    # cv2.imshow('green', green_mask)
+    return (green_mask, red_mask)
+
+def check_arrow(masks, frame, aruco_center):
+    green_mask, red_mask = masks
+    green_contours, _ = cv2.findContours(green_mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    red_contours, _ = cv2.findContours(red_mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    # left green arrow
+    for contour in green_contours:
+        if cv2.contourArea(contour) > 500:
+            x, y, w, h = cv2.boundingRect(contour)
+            cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 0, 255), 2)
+            cv2.putText(frame, 'LEFT', (x, y-10), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (0, 0, 255), 2)
+            if x < aruco_center[0]:
+                return 0
+    #right red arrow
+    for contour in red_contours:
+        if cv2.contourArea(contour) > 500:
+            x, y, w, h = cv2.boundingRect(contour)
+            cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 0, 255), 2)
+            cv2.putText(frame, 'RIGHT', (x, y-10), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (0, 0, 255), 2)
+            if x > aruco_center[0]:
+                return 1
+    return -1
+
+def find_center(corners):
+    """
+    Calculate the center coordinates of an ArUco marker.
+
+    Args:
+        corners (list): A list of corner points of the detected ArUco marker.
+
+    Returns:
+        tuple: The (x, y) coordinates of the marker's center.
+    """
+    for outline in corners:
+        marker_corners = outline.reshape((4,2))
+        
+        # Compute the center of the marker
+        center_x = int(np.mean(marker_corners[:, 0]))
+        center_y = int(np.mean(marker_corners[:, 1]))
+
+    return (center_x, center_y)
+
+def distance(corners, ids, frame, center):
+        _, tvecs, _ = aruco.estimatePoseSingleMarkers(corners, MARKER_WIDTH_IRL, cameraMatrix, dist)
+        if ids is not None:
+            for i in range(len(ids)):
+                # Extract translation vector (tvec) to get distance
+                distance_found = tvecs[i][0][2]  # Z-distance from camera to marker
+
+                # Display distance on the image
+                cv2.putText(frame, f"{distance_found:.2f} inches", (center[0] + 10, center[1] - 15), cv2.FONT_HERSHEY_SIMPLEX, 0.6, (0, 0, 255), 2)
+                # debug
+                print(f"Marker ID {ids[i][0]}: {distance_found:.2f} inches")
+                return distance_found
+                
 def main():
     # put all functionality here
+    cap = cv2.VideoCapture(0)
+    if not cap.isOpened():
+        print("Error: Could not open camera.")
+        return
+    #camera warm up
+    time.sleep(2)
+    for _ in range(5):
+        cap.read()
+
+    myDict = aruco.getPredefinedDictionary(aruco.DICT_6X6_50)
+    while True:
+        ret, frame = cap.read()
+        if not ret:
+            print("Error: Failed to capture image.")
+            break
+        # Convert the image to grayscale then apply adaptive threshold that helps exemplify contours for aruco detection
+        
+        frame_undistorted = cv2.undistort(frame, cameraMatrix, dist)
+        gray = cv2.cvtColor(frame_undistorted, cv2.COLOR_BGR2GRAY)
+        corners, ids, _ = aruco.detectMarkers(gray, myDict)
+        if len(corners) > 0:
+            frame = aruco.drawDetectedMarkers(frame, corners, borderColor=(0,255,255))
+            center = find_center(corners)
+            cv2.circle(frame, center, 3, (255, 255, 0), -1)
+            masks = find_mask(frame)
+            arrow = check_arrow(masks, frame, center)
+            if arrow == 0:
+                #here we would modify instruction array
+                print("LEFT")
+            elif arrow == 1:
+                print("RIGHT")
+            else:
+                #no arrow detected good_arrow ->0.0
+                print("NO ARROW DETECTED")
+            distance(corners, id, frame_undistorted, center)
+        cv2.imshow('Demo2', frame_undistorted)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+    #turn off the camera and destroy all windows
+    cap.release()
+    cv2.destroyAllWindows()
     return
 
 #run the code
