91e8bfac8d0489944eb9e03745ad69ea8f2bee9f
kobeprior99 <129470871+kobeprior99@users.noreply.github.com>
2025-03-24 21:01:24 -0600

    changed last instructions

--- Demo2/Computer_Vision/Demo2.py
+++ Demo2/Computer_Vision/Demo2.py
@@ -373,7 +373,7 @@ def main():
         #send instructions to arduino
         if instructions != last_instructions:
             send_instructions()
-        last_instruction = instructions
+        last_instructions = instructions
         #send instructions to LCD
         with data_lock:
             if instructions[0] == 1.0:
